b80eab95da35ca26d3d6788465b32d7a2a02d5d9
pratek7 <pratikjungkarki123@gmail.com>
2023-01-26 22:19:20 +0545

    configed and updateded package

--- src/App.js
+++ src/App.js
@@ -3,19 +3,27 @@ import Header from "./Header";
 import "./App.css";
 import SideBar from "./SideBar";
 import Feed from "./Feed";
+import { useSelector } from "react-redux";
+import { selectUser } from "./features/userSlice";
+import Login from "./Login";
 
 function App() {
+  const user = useSelector(selectUser);
   return (
     <div className="App">
       {/* header  */}
       <Header />
+      {!user ? (
+        <Login />
+      ) : (
+        <div className="app__body">
+          <SideBar />
+          {/* feed  */}
+          <Feed />
+          {/* widgets */}
+        </div>
+      )}
       {/* sidebar  */}
-      <div className="app__body">
-        <SideBar />
-        {/* feed  */}
-        <Feed />
-        {/* widgets */}
-      </div>
     </div>
   );
 }
---
+++ src/Login.js
@@ -0,0 +1,75 @@
+import { Password } from "@mui/icons-material";
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { login } from "./features/userSlice";
+import { auth } from "./Firebase";
+import "./Login.css";
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [profile, setProfile] = useState("");
+  const dispatch = useDispatch();
+  const LoginToApp = (e) => {
+    e.preventDefault();
+  };
+  const register = () => {
+    if (!name) {
+      return alert("enter a full name");
+    }
+    auth
+      .createUserWithEmailAndPassword(email, Password)
+      .then((userAuth) => {
+        userAuth.user
+          .updateProfile({
+            displayName: name,
+            photoURL: profile,
+          })
+          .then(() => {
+            dispatch(
+              login({
+                email: userAuth.user.email,
+                uid: userAuth.user.uid,
+                displayName: name,
+                photoURL: profile,
+              })
+            );
+          });
+      })
+      .catch((err) => alert(err));
+  };
+  return (
+    <div className="login">
+      <img src="" alt="" />
+      <form>
+        <input
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          type="text"
+          placeholder="Full name (requiired if registering)"
+        />
+        <input
+          value={profile}
+          onChange={(e) => setProfile(e.target.value)}
+          type="text"
+          placeholder="Profile pic URL (optionals)"
+        />
+        <input
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          type="email"
+          placeholder="Email"
+        />
+        <input type="password" placeholder="Password" />
+        <button type="submit" onClick={LoginToApp}>
+          Sign In
+        </button>
+      </form>
+      <p>Not a member?</p>
+      <span className="login__register" onClick={register}>
+        Register Now
+      </span>
+    </div>
+  );
+};
+
+export default Login;
